e455ba153ffee163eb92994b1c84a04cff8867fa
Macinstosh <chhnsn@macbook-air-de-macinstosh.local>
2019-01-13 13:32:50 +0100

    Initial Commit

---
+++ AppareilPhoto2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppareilPhoto2
+//
+//  Created by Macinstosh on 13/01/2019.
+//  Copyright © 2019 ozvassilius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
